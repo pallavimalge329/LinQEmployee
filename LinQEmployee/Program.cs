0e503eecc9a30ce353c78f81e6327bcd41cbb27b
pallavimalge329 <malgepallavi21@gmail.com>
2021-09-13 17:20:19 +0530

    Add project files.

---
+++ LinQEmployee/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = "DESKTOP-N5QBMVO\SQLEXPRESS";
+        }
+    }
+}
